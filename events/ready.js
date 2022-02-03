4b8759f9615702e7c1490dde9a5d45388e455f9f
CLOUDZiii <98861952+CLOUDZiii@users.noreply.github.com>
2022-02-03 10:04:59 -0500

    Created basic command & event handler

---
+++ events/ready.js
@@ -0,0 +1,11 @@
+module.exports = {
+    name: "ready",
+    run: async (bot) => {
+        console.log("")
+        console.log("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+        console.log("")
+        console.log("Logged in as " + bot.client.user.tag)
+        console.log("")
+        console.log("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+    }
+}
--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 const Discord = require("discord.js")
-
 require("dotenv").config()
 
 const client = new Discord.Client({
-    intents: ["GUILDS"]
+    intents: [
+        "GUILDS",
+        "GUILD_MESSAGES",
+        "GUILD_MEMBERS"
+    ]
 })
 
-client.on("ready", () =>{
-    console.log(`Logged in as ${client.user.tag}`)
-})
+let bot = {
+    client, 
+    prefix: "p?",
+    owners: ["937850651835265055"]
+}
+
+client.commands = new Discord.Collection()
+client.events = new Discord.Collection()
+
+client.loadEvents = (bot, reload) => require("./handlers/events")(bot, reload)
+client.loadCommands = (bot, reload) => require("./handlers/commands")(bot, reload)
+
+client.loadEvents(bot, false)
+client.loadCommands(bot, false)
+
+module.exports = bot
+
 
 client.login(process.env.TOKEN)
